82fb76d69b575f520eb4c8b9e80c9cec95eaea02
axa880 <axa880@gmail.com>
2018-09-12 12:57:10 -0400

    BDD file update2

--- assignments/test_awscreds.py
+++ assignments/test_awscreds.py
@@ -15,7 +15,169 @@ def execute(cmd):
 
 class test_awscreds(unittest.TestCase):
   def setUp(self):
-    execute('./setup-assignments')
+    execute('sudo ./setup-assignments')
 
-  def test_awscreds_returns_access_key_given_profile(self):
+# Tests for Profile: default
+  def test_awscreds_returns_access_key_default_field_and_default_profile(self):
+		self.assertIn('AKAIISDAIK5LJWV264SAGX7XNA', execute('./awscreds'))
+
+  def test_awscreds_returns_access_key_default_profile_and_field_key(self):
+		self.assertIn('AKAIISDAIK5LJWV264SAGX7XNA', execute('./awscreds -field key'))
+
+  def test_awscreds_returns_access_key_default_profile_and_field_secret(self):
+		self.assertIn('dxt7e3d3fCSf/NftN8Cys5L4duQ2KPQwN0n+oW', execute('./awscreds -field secret'))
+
+# Since substring comparison passes, verification should be for the both whole strings
+  def test_awscreds_returns_access_key_default_profile_and_field_both(self):
+		self.assertIn('AKAIISDAIK5LJWV264SAGX7XNA\r\ndxt7e3d3fCSf/NftN8Cys5L4duQ2KPQwN0n+oW', execute('./awscreds -field both'))
+
+
+# Tests for Profile: profile1
+  def test_awscreds_returns_access_key_default_field_and_profile1(self):
+		self.assertIn('testvalue', execute('./awscreds -profile profile1'))
+
+  def test_awscreds_returns_access_key_profile_profile1_and_field_key(self):
 		self.assertIn('testvalue', execute('./awscreds -field key -profile profile1'))
+
+  def test_awscreds_returns_access_key_profile_profile1_and_field_secret(self):
+		self.assertIn('testsecretvalue', execute('./awscreds -field secret -profile profile1'))
+
+# Since substring comparison passes, verification should be for the both whole strings
+  def test_awscreds_returns_access_key_profile_profile1_and_field_both(self):
+		self.assertIn('testvalue\r\ntestsecretvalue', execute('./awscreds -field both -profile profile1'))
+
+# Note: Tests for flags/switchs changeed their positions (like, "-profile" first and then "-field")
+# Tests for Profile: profile1
+  def test_awscreds_returns_access_key_profile_profile1_and_field_key_flags_switched(self):
+		self.assertIn('testvalue', execute('./awscreds -profile profile1 -field key'))
+
+  def test_awscreds_returns_access_key_profile_profile1_and_field_secret_flags_switched(self):
+		self.assertIn('testsecretvalue', execute('./awscreds -profile profile1 -field secret'))
+
+  def test_awscreds_returns_access_key_profile_profile1_and_field_both_flags_switched(self):
+		self.assertIn('testvalue\r\ntestsecretvalue', execute('./awscreds -profile profile1 -field both'))
+
+
+# Tests for Profile: profile2
+  def test_awscreds_returns_access_key_default_field_and_profile2(self):
+		self.assertIn('', execute('./awscreds -profile profile2'))
+
+  def test_awscreds_returns_access_key_profile_profile2_and_field_key(self):
+		self.assertIn('', execute('./awscreds -field key -profile profile2'))
+
+  def test_awscreds_returns_access_key_profile_profile2_and_field_secret(self):
+		self.assertIn('', execute('./awscreds -field secret -profile profile2'))
+
+  def test_awscreds_returns_access_key_profile_profile2_and_field_both(self):
+		self.assertIn('', execute('./awscreds -field both -profile profile2'))
+
+
+# Tests for Profile: profile3
+# Note: Not a very strong test, tried to do for Python specific tests for the null value
+  def test_awscreds_returns_access_key_default_field_and_profile3(self):
+		self.assertIn('None', execute('./awscreds -profile profile3'))
+
+  def test_awscreds_returns_access_key_profile_profile3_and_field_key(self):
+		self.assertIn('None', execute('./awscreds -field key -profile profile3'))
+
+  def test_awscreds_returns_access_key_profile_profile3_and_field_secret(self):
+		self.assertIn('None', execute('./awscreds -field secret -profile profile3'))
+
+  def test_awscreds_returns_access_key_profile_profile3_and_field_both(self):
+		self.assertIn('None\r\nNone', execute('./awscreds -field both -profile profile3'))
+
+
+# Tests for Profile: profile4
+# BUG: All these 4 tests pass, it should be a BUG, because values should not be allowed with 1 character long
+  def test_awscreds_returns_access_key_default_field_and_profile4(self):
+		self.assertIn('x', execute('./awscreds -profile profile4'))
+
+  def test_awscreds_returns_access_key_profile_profile4_and_field_key(self):
+		self.assertIn('x', execute('./awscreds -field key -profile profile4'))
+
+  def test_awscreds_returns_access_key_profile_profile4_and_field_secret(self):
+		self.assertIn('y', execute('./awscreds -field secret -profile profile4'))
+
+  def test_awscreds_returns_access_key_profile_profile4_and_field_both(self):
+		self.assertIn('x\r\ny', execute('./awscreds -field both -profile profile4'))
+
+
+# Tests for Profile: profile5
+  def test_awscreds_returns_access_key_default_field_and_profile5(self):
+		self.assertIn('testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5', execute('./awscreds -profile profile5'))
+
+  def test_awscreds_returns_access_key_profile_profile5_and_field_key(self):
+		self.assertIn('testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5', execute('./awscreds -field key -profile profile5'))
+
+  def test_awscreds_returns_access_key_profile_profile5_and_field_secret(self):
+		self.assertIn('testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5', execute('./awscreds -field secret -profile profile5'))
+
+  def test_awscreds_returns_access_key_profile_profile5_and_field_both(self):
+		self.assertIn('testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5testvalue5\r\ntestsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5testsecretvalue5', execute('./awscreds -field both -profile profile5'))
+
+
+# Tests for Profile: profile6
+  def test_awscreds_returns_access_key_default_field_and_profile6(self):
+		self.assertIn('testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6', execute('./awscreds -profile profile6'))
+
+  def test_awscreds_returns_access_key_profile_profile6_and_field_key(self):
+		self.assertIn('testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6', execute('./awscreds -field key -profile profile6'))
+
+  def test_awscreds_returns_access_key_profile_profile6_and_field_secret(self):
+		self.assertIn('testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6', execute('./awscreds -field secret -profile profile6'))
+
+  def test_awscreds_returns_access_key_profile_profile6_and_field_both(self):
+		self.assertIn('testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6testvalue6\r\ntestsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6testsecretvalue6', execute('./awscreds -field both -profile profile6'))
+
+
+# Tests for Profile: profile7
+  def test_awscreds_returns_access_key_default_field_and_profile7(self):
+# Characters after "#" is getting ignored, this is Pyhton specific syntax issue, not a product BUG
+		self.assertIn('!@', execute('./awscreds -profile profile7'))
+
+  def test_awscreds_returns_access_key_profile_profile7_and_field_key(self):
+# Characters after "#" is getting ignored, this is Pyhton specific syntax issue, not a product BUG
+		self.assertIn('!@', execute('./awscreds -field key -profile profile7'))
+
+  def test_awscreds_returns_access_key_profile_profile7_and_field_secret(self):
+		self.assertIn('&*()_+', execute('./awscreds -field secret -profile profile7'))
+
+  def test_awscreds_returns_access_key_profile_profile7_and_field_both(self):
+# Characters after "#" is getting ignored, this is Pyhton specific syntax issue, not a product BUG
+		self.assertIn('!@\r\n&*()_+', execute('./awscreds -field both -profile profile7'))
+
+
+# Tests for Profile: profile8
+  def test_awscreds_returns_access_key_default_field_and_profile8(self):
+		self.assertIn('/n/t/n/t///', execute('./awscreds -profile profile8'))
+
+  def test_awscreds_returns_access_key_profile_profile8_and_field_key(self):
+		self.assertIn('/n/t/n/t///', execute('./awscreds -field key -profile profile8'))
+
+  def test_awscreds_returns_access_key_profile_profile8_and_field_secret(self):
+		self.assertIn('/n/t/n/t////////', execute('./awscreds -field secret -profile profile8'))
+
+  def test_awscreds_returns_access_key_profile_profile8_and_field_both(self):
+		self.assertIn('/n/t/n/t///\r\n/n/t/n/t////////', execute('./awscreds -field both -profile profile8'))
+
+# Tests for Profile: profile9
+# Following 4 tests run fine manually from the CLI; but for automation with Python, beginning 
+# the substirng "'''" works as starting of the Python block comment, this is Python's limitation
+# also wraping "'''" with more double quotes and single quotes gives "~" a syntax error
+'''
+  def test_awscreds_returns_access_key_default_field_and_profile9(self):
+		self.assertIn('~````````''''''12345', execute('./awscreds -profile profile9'))
+
+  def test_awscreds_returns_access_key_profile_profile9_and_field_key(self):
+		self.assertIn('~````````''''''12345', execute('./awscreds -field key -profile profile9'))
+
+  def test_awscreds_returns_access_key_profile_profile9_and_field_secret(self):
+		self.assertIn('~`````````````''''''abcdefg', execute('./awscreds -field secret -profile profile9'))
+
+  def test_awscreds_returns_access_key_profile_profile9_and_field_both(self):
+		self.assertIn('~````````''''''12345\r\n~`````````````''''''abcdefg', execute('./awscreds -field both -profile profile9'))
+'''
+
+# End of file
+
+
--- assignments/test_syntax_validator.py
+++ assignments/test_syntax_validator.py
@@ -1,15 +1,62 @@
 import unittest
+from ddt import ddt, data
 import syntax_validator
 
+@ddt
 class test_syntax_validator(unittest.TestCase):
-    def test_is_valid_username_returns_true_given_valid_input(self):
-        self.assertTrue(syntax_validator.is_valid_username('Abcdefg1'))
 
-    def test_is_valid_username_returns_false_given_invalid_input(self):
-        self.assertFalse(syntax_validator.is_valid_username('abcdefg'))
+# User name test cases
+    @data("Abcdefg1", "Abcdefg12", "Abcdefg123", "Abcdefg1234", "Abcdefg12345", "Z1234567890a", "A1b2c3d4e5d6")
+    def test_is_valid_username_returns_true_given_valid_input_within_the_range(self, username):
+        self.assertTrue(syntax_validator.is_valid_username(username))
 
-    def test_is_valid_us_zip_code_returns_true_given_valid_input(self):
-        self.assertTrue(syntax_validator.is_valid_us_zip_code('55555-5555'))
+    @data("", "A", "A1", "A12", "A123", "A1234", "B12345", "C123456", "Z123456789000", "O0000000000000")
+    def test_is_valid_username_returns_false_given_invalid_input_outside_the_range(self, username):
+        self.assertFalse(syntax_validator.is_valid_username(username))
 
-    def test_is_valid_us_zip_code_returns_false_given_invalid_input(self):
-        self.assertFalse(syntax_validator.is_valid_us_zip_code('5555'))
+    @data("Hbcdefg1", "O00000000", "Xb8defg123", "Ubcde9g1s34", "Sb0defg12345", "M1234t678j0a", "V1b2c3d4e5d6")
+    def test_is_valid_username_returns_true_given_valid_input_contains_alpha_numeric(self, username):
+        self.assertTrue(syntax_validator.is_valid_username(username))
+
+# These tests can be expanded to many more combinations, but I just stopped here.
+    @data("", "!", "$1", "A@", "A%23", "A1(34", "B12*45", "________", "++++++", ">", "123_abc", "|||||||||", "^", "%i)")
+    def test_is_valid_username_returns_false_given_invalid_input_contains_non_alpha_numeric(self, username):
+        self.assertFalse(syntax_validator.is_valid_username(username))
+
+    @data("Abcdefg1", "Abcdefg12", "Abcdefg123", "Abcdefg1234", "A12345bcdefg", "O12345678900")
+    def test_is_valid_username_returns_true_given_valid_input_contains_1_or_more_digit(self, username):
+        self.assertTrue(syntax_validator.is_valid_username(username))
+
+# BUG FOUND - All of these tests should pass as negaive tests, but they are failing.
+# The code is failing to find out that there is not a single digit in the user names. 
+    @data("Aaaaaaaa", "Abasdfghj", "Addddddddd", "Akjhgfdefgh", "Abcdefghijkl")
+    def test_is_valid_username_returns_false_given_invalid_input_contains_no_digit(self, username):
+        self.assertFalse(syntax_validator.is_valid_username(username))
+
+    @data("Abcdefg1", "Abcdefg12", "Abcdefg123", "Abcdefg1234", "Abcdefg12345", "Z1234567890a", "A1b2c3d4e5d6")
+    def test_is_valid_username_returns_true_given_valid_input_starts_with_capital_letter(self, username):
+        self.assertTrue(syntax_validator.is_valid_username(username))
+
+    @data("abcdefg1", "bbcdefg12", "cbcdefg123", "dbcdefg1234", "obcdefg12345")
+    def test_is_valid_username_returns_false_given_invalid_input_starts_with_no_capital_letter(self, username):
+        self.assertFalse(syntax_validator.is_valid_username(username))
+
+# ZIP code test cases
+    @data("12345", "13579", "55555", "00000", "01230", "98765", "99999")
+    def test_is_valid_us_zip_code_returns_true_given_valid_input_contains_only_5digit(self, zipcode):
+        self.assertTrue(syntax_validator.is_valid_us_zip_code(zipcode))
+
+    @data("", "1", "13", "555", "0000", "101230", "987658888", "NotMyZip", "-1234", "%^&*(")
+    def test_is_valid_us_zip_code_returns_false_given_invalid_input_contains_non_5digit(self, zipcode):
+        self.assertFalse(syntax_validator.is_valid_us_zip_code(zipcode))
+
+    @data("12345-6789", "13579-2468", "55555-5555", "00000-0000", "01230-4500", "98765-4321", "99999-9999")
+    def test_is_valid_us_zip_code_returns_true_given_valid_input_contains_5digit_4digit(self, zipcode):
+        self.assertTrue(syntax_validator.is_valid_us_zip_code(zipcode))
+
+    @data("-", "1-1", "1_3", "555-555", "0o00000-0000000", "10123-12345", "98765-*888", "NotMyzip", "-1234", "%^&*-&*")
+    def test_is_valid_us_zip_code_returns_false_given_invalid_input_contains_non_5digit_4digit(self, zipcode):
+        self.assertFalse(syntax_validator.is_valid_us_zip_code(zipcode))
+
+# End of File - Out of total 95 tests, 5 tests are failing for a bug - Ashfaque Ahammad
+
